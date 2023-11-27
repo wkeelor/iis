2b0bf2d1a8dae987412aa5a93a1093929913f913
valkovska <valkovska@mathesio.cz>
2023-11-27 23:35:59 +0100

    edit icon

--- iis/resources/views/events/event.blade.php
+++ iis/resources/views/events/event.blade.php
@@ -61,9 +61,9 @@
                             @endif
 
                             <button type="submit" class="inline-flex items-center justify-center rounded-md border-2 border-transparent bg-gray-900 bg-none px-12 py-3 text-center text-xs font-bold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-gray-800">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 mr-3 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <i class="fa-solid fa-basket-shopping fa-xl hover:text-blue-700 dark:hover:text-blue-500">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-                                </svg>
+                                </i>
                             <!--Pridať do košíku-->
                             </button>
                         </div>
